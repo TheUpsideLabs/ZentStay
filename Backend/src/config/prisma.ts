bb1d71febe1169a9b637d055776584722cdc82dc
Prateek Kumar Chauhan <246912639+prateek-chauhan27@users.noreply.github.com>
2026-09-02 18:23:15 +0530

    updated

--- Backend/src/config/prisma.ts
+++ Backend/src/config/prisma.ts
@@ -1,5 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: process.env.DATABASE_URL
+    ? {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      }
+    : undefined,
+});
 
-export default prisma;
+export default prisma;
